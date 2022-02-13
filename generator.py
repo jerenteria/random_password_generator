893a732c8af93c3b2eeb1699df34697a03a846e5
jerenteria <renteria.juan96@gmail.com>
2022-02-13 11:19:52 -0800

    finished

--- generator.py
+++ generator.py
@@ -2,10 +2,13 @@ import random, string
 
 # sets var letters to print out randmom string using .ascii_letters
 # .ascii_letters prints out letters in both upper case and lower case
-letters = string.ascii_letters
-print ( ''.join(random.choice(letters) for i in range(10)) )
 
-## generates random ingeter from 0-5
+letters = string.ascii_letters
+print ( ''.join(random.choice(letters) for i in range(10)))
+
+
+
+generates random ingeter from 0-5
 n = random 
 n = random.randint(0,5)
 print(n)
